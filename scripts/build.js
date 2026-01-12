8278e847080c7b21795cefcb31cae477a6c226d2
Robert <robertday778@gmail.com>
2026-01-12 21:37:37 +0000

    Use env var for 1st Phorm affiliate link

--- scripts/build.js
+++ scripts/build.js
@@ -13,6 +13,20 @@ function copyFile(src, dest) {
   fs.copyFileSync(src, dest);
 }
 
+function copySupplementsWithEnvLink(src, dest) {
+  const affiliateLink = process.env.NEXT_PUBLIC_FIRST_PHORM_LINK;
+  if (!affiliateLink) {
+    console.error('Missing env var: NEXT_PUBLIC_FIRST_PHORM_LINK');
+    console.error('Set it to your Impact affiliate URL before running the build.');
+    process.exit(1);
+  }
+
+  const html = fs.readFileSync(src, 'utf8');
+  const updated = html.replaceAll('__FIRST_PHORM_LINK__', affiliateLink);
+  ensureDir(path.dirname(dest));
+  fs.writeFileSync(dest, updated, 'utf8');
+}
+
 function copyDir(srcDir, destDir) {
   if (!fs.existsSync(srcDir)) return;
   ensureDir(destDir);
@@ -32,7 +46,10 @@ function copyDir(srcDir, destDir) {
 ensureDir(dist);
 
 copyFile(path.join(root, 'index.html'), path.join(dist, 'index.html'));
-copyFile(path.join(root, 'supplements', 'index.html'), path.join(dist, 'supplements', 'index.html'));
+copySupplementsWithEnvLink(
+  path.join(root, 'supplements', 'index.html'),
+  path.join(dist, 'supplements', 'index.html')
+);
 copyDir(path.join(root, 'assets'), path.join(dist, 'assets'));
 
 console.log('Build complete: dist/');
